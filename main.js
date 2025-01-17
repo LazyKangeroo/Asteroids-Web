96e5b1bd52e7c8f0fbef3bb2d01d2c9958263df5
Lazykangeroo <fvantonder00@gmail.com>
2025-01-17 16:52:28 +0200

    Fixing up
    Simplifying HTML-file
    Removing unfunctional JS-code
    Deleted 'object_class.js' file because it was unneeded

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-const { request } = require("express");
-
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
@@ -7,50 +5,28 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 
+// function eventHandler() {
+//     document.addEventListener('keydown', (e) => {
+//         switch(e.key) {
+//             case 'ArrowUp':
+//                 player.updatePosition(0, -player.speed);
+//                 break;
+//             case 'ArrowDown':
+//                 player.updatePosition(0, player.speed);
+//                 break;
+//             case 'ArrowLeft':
+//                 player.updatePosition(-player.speed, 0);
+//                 break;
+//             case 'ArrowRight':
+//                 player.updatePosition(player.speed, 0);
+//                 break;
+//         }
+//     });
+// }
 
-function gameLoop() {
-    // Clear canvas
-    ctx.clearRect(0,0, canvas.width, canvas.height);
-
-    // draw game objs
-    drawPlayer();
-    drawAsteroids();
-
-    updateGame();
-
-    // requisting next animation frame
-    requestAnimationFrame(gameLoop);
-}
-
-
-function eventHandler() {
-    document.addEventListener('keydown', (e) => {
-        switch(e.key) {
-            case 'ArrowUp':
-                player.updatePosition(0, -player.speed);
-                break;
-            case 'ArrowDown':
-                player.updatePosition(0, player.speed);
-                break;
-            case 'ArrowLeft':
-                player.updatePosition(-player.speed, 0);
-                break;
-            case 'ArrowRight':
-                player.updatePosition(player.speed, 0);
-                break;
-        }
-    });
-}
-
-function startGame() {
-    gameLoop();
-}
 
 // Event listener for window resize
 window.addEventListener('resize', () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 });
-
-// Start the game when the page loads
-startGame();
