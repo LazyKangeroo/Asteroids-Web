2611b7d2245aaf477c8791ea0dbf4f69af28e2a9
Lazykangeroo <fvantonder00@gmail.com>
2025-01-18 14:49:13 +0200

    Player and Ast collition

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ class Player {
         this.pos = pos // (x,y)
         this.vel = vel
         this.rotation = 0
+        this.score = 0
     }
 
     draw() {
@@ -35,6 +36,25 @@ class Player {
         this.pos.x += this.vel.x
         this.pos.y += this.vel.y
     }
+
+    getVertices() {
+        const cos = Math.cos(this.rotation)
+        const sin = Math.sin(this.rotation)
+        return [
+          {
+            x: this.pos.x + cos * 30 - sin * 0,
+            y: this.pos.y + sin * 30 + cos * 0,
+          },
+          {
+            x: this.pos.x + cos * -15 - sin * 15,
+            y: this.pos.y + sin * -15 + cos * 15,
+          },
+          {
+            x: this.pos.x + cos * -15 - sin * -15,
+            y: this.pos.y + sin * -15 + cos * -15,
+          },
+        ]
+      }
 }
 
 class Asteroids {
@@ -42,7 +62,22 @@ class Asteroids {
         this.pos = pos
         this.vel = vel
         this.r = r
-        // this.rotation = rotation
+
+        this.points = {
+            combos : {
+                sniper : 120, // shooting small ones
+                rapit : 80, // destroying large ones within 300mm
+                close_calls : {
+                    dodge : 150,
+                    last_minute : 200
+                }
+            },
+            damage : {
+                small : 20,
+                meduim : 40,
+                large : 60
+            }
+        }
     }
 
     draw() {
@@ -57,6 +92,10 @@ class Asteroids {
         this.draw()
         this.pos.x += this.vel.x
         this.pos.y += this.vel.y
+    }
+
+    scoreCal(asteroid, projectile) {
+
     }
 }
 
@@ -152,36 +191,38 @@ const asteroids = []
 const AST_MX_SPEED = 5
 const AST_MIN_SPEED = 1
 
-window.setInterval(() => {
+const intervalId = window.setInterval(() => {
     const index = Math.floor(Math.random() * 4)
     let x, y
     let vx, vy
-    let r = 50 * Math.random() + 10
+    const r = 50 * Math.random() + 15
+    const speed = Math.random() * (AST_MX_SPEED - AST_MIN_SPEED) + AST_MIN_SPEED
 
     switch (index) {
       case 0: // left side of the screen
         x = 0 - r
         y = Math.random() * canvas.height
-        vx = 1
+        vx = speed
         vy = 0
+
         break
       case 1: // bottom side of the screen
         x = Math.random() * canvas.width
         y = canvas.height + r
         vx = 0
-        vy = -1
+        vy = -speed
         break
       case 2: // right side of the screen
         x = canvas.width + r
         y = Math.random() * canvas.height
-        vx = -1
+        vx = -speed
         vy = 0
         break
       case 3: // top side of the screen
         x = Math.random() * canvas.width
         y = 0 - r
         vx = 0
-        vy = 1
+        vy = speed
         break
     }
     asteroids.push(
@@ -197,11 +238,11 @@ window.setInterval(() => {
         r,
       })
     )
-  }, 1000)
+  }, Math.floor(Math.random() * (1500 - 200) + 200 ))
 
 
 function animate() {
-    window.requestAnimationFrame(animate)
+    const animationId = window.requestAnimationFrame(animate)
     ctx.fillStyle = 'black'
     ctx.fillRect(0,0,canvas.width,canvas.height)
 
@@ -221,11 +262,29 @@ function animate() {
         const asteroid = asteroids[i]
         asteroid.update()
 
-        if (asteroid.pos.x + asteroid.r < 0 || asteroid.pos.x > canvas.width || asteroid.pos.y + asteroid.r < 0 || asteroid.pos.y > canvas.height) {
+        // out of screen
+       if (asteroid.pos.x + asteroid.r < 0 || asteroid.pos.x > canvas.width || asteroid.pos.y + asteroid.r < 0 || asteroid.pos.y > canvas.height) {
             asteroids.splice(i,1)
         }
-    }
 
+        if (circleTriangleCollision(asteroid, player.getVertices())) {
+            console.log('GAME OVER')
+            window.cancelAnimationFrame(animationId)
+            clearInterval(intervalId)
+          }
+
+        // Collition
+        for (let j = projectiles.length - 1; j >= 0; j--) {
+            const projectile = projectiles[j]
+            if (circleCollition(asteroid, projectile)) {
+                console.log('Hit');
+                projectiles.splice(j,1)
+                // Sizing asteroid
+                if (asteroid.r <= 20) asteroids.splice(i,1)
+                    else if (asteroid.r >= 20) { asteroid.r = asteroid.r / 2 }
+            }
+        }
+    }
     // Player movement
     if (keys.w.pressed) {
         player.vel.x = Math.cos(player.rotation) * SPEED
@@ -237,5 +296,61 @@ function animate() {
     if (keys.d.pressed) player.rotation += ROTATION
     else if (keys.a.pressed) player.rotation -= ROTATION
 }
+
+function circleCollition(circle1,circle2) {
+    const xDistance = circle1.pos.x - circle2.pos.x
+    const yDistance = circle1.pos.y - circle2.pos.y
+
+    const Distance = Math.sqrt(xDistance ** 2 + yDistance ** 2)
+
+    if (Distance < circle1.r + circle2.r) {
+        return true
+    }
+    return false
+}
+
+function circleTriangleCollision(circle, triangle) {
+    // Check if the circle is colliding with any of the triangle's edges
+    for (let i = 0; i < 3; i++) {
+      let start = triangle[i]
+      let end = triangle[(i + 1) % 3]
+
+      let dx = end.x - start.x
+      let dy = end.y - start.y
+      let length = Math.sqrt(dx * dx + dy * dy)
+
+      let dot = ((circle.pos.x - start.x) * dx +
+          (circle.pos.y - start.y) * dy) /
+        Math.pow(length, 2)
+
+      let closestX = start.x + dot * dx
+      let closestY = start.y + dot * dy
+
+      if (!isPointOnLineSegment(closestX, closestY, start, end)) {
+        closestX = closestX < start.x ? start.x : end.x
+        closestY = closestY < start.y ? start.y : end.y
+      }
+
+      dx = closestX - circle.pos.x
+      dy = closestY - circle.pos.y
+
+      let distance = Math.sqrt(dx * dx + dy * dy)
+
+      if (distance <= circle.r) {
+        return true
+      }
+    }
+    // No collision
+    return false
+  }
+
+  function isPointOnLineSegment(x, y, start, end) {
+    return (
+      x >= Math.min(start.x, end.x) &&
+      x <= Math.max(start.x, end.x) &&
+      y >= Math.min(start.y, end.y) &&
+      y <= Math.max(start.y, end.y)
+    )
+  }
 
 animate()
