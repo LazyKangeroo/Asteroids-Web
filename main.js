8c34da4eb2507b94c88d549a692952256d5705e4
Lazykangeroo <fvantonder00@gmail.com>
2025-01-18 12:24:58 +0200

    Spawing Basic Asteroids
    Come from all directions
    TODO :  Angles and bouncing off one another

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ canvas.height = window.innerHeight
 
 class Player {
     constructor({ pos, vel }) {
-        this.pos= pos // (x,y)
+        this.pos = pos // (x,y)
         this.vel = vel
         this.rotation = 0
     }
@@ -28,6 +28,29 @@ class Player {
         ctx.stroke()
 
         ctx.restore()
+    }
+
+    update() {
+        this.draw()
+        this.pos.x += this.vel.x
+        this.pos.y += this.vel.y
+    }
+}
+
+class Asteroids {
+    constructor({pos, vel, r}) {
+        this.pos = pos
+        this.vel = vel
+        this.r = r
+        // this.rotation = rotation
+    }
+
+    draw() {
+        ctx.beginPath()
+        ctx.arc(this.pos.x,this.pos.y,this.r,0,Math.PI * 2, false)
+        ctx.closePath()
+        ctx.strokeStyle = 'white'
+        ctx.stroke()
     }
 
     update() {
@@ -60,8 +83,8 @@ class Projectiles {
 }
 
 const player = new Player({
-    vel : {x : 0, y:0},
-    pos : {x : canvas.width / 2, y : canvas.height/ 2}
+    vel : {x : 0, y : 0},
+    pos : {x : canvas.width / 2, y : canvas.height / 2}
 })
 
 const keys = {
@@ -100,7 +123,6 @@ window.addEventListener('keydown', (event) => {
                     y : Math.sin(player.rotation) * PROJECTILE_SPEED
                 }
             }))
-            console.log(projectiles)
             break
     }
 })
@@ -121,10 +143,62 @@ window.addEventListener('keyup', (event) => {
 
 const FRICTION = 0.97
 const SPEED = 3
-const ROTATION = 0.05
+const ROTATION = 0.04
 
 const projectiles = []
 const PROJECTILE_SPEED = 8
+
+const asteroids = []
+const AST_MX_SPEED = 5
+const AST_MIN_SPEED = 1
+
+window.setInterval(() => {
+    const index = Math.floor(Math.random() * 4)
+    let x, y
+    let vx, vy
+    let r = 50 * Math.random() + 10
+
+    switch (index) {
+      case 0: // left side of the screen
+        x = 0 - r
+        y = Math.random() * canvas.height
+        vx = 1
+        vy = 0
+        break
+      case 1: // bottom side of the screen
+        x = Math.random() * canvas.width
+        y = canvas.height + r
+        vx = 0
+        vy = -1
+        break
+      case 2: // right side of the screen
+        x = canvas.width + r
+        y = Math.random() * canvas.height
+        vx = -1
+        vy = 0
+        break
+      case 3: // top side of the screen
+        x = Math.random() * canvas.width
+        y = 0 - r
+        vx = 0
+        vy = 1
+        break
+    }
+    asteroids.push(
+      new Asteroids({
+        pos: {
+          x: x,
+          y: y,
+        },
+        vel: {
+          x: vx,
+          y: vy,
+        },
+        r,
+      })
+    )
+  }, 1000)
+
 
 function animate() {
     window.requestAnimationFrame(animate)
@@ -133,6 +207,7 @@ function animate() {
 
     player.update();
 
+    // Projectile management
     for (let i = projectiles.length - 1; i >= 0; i--) {
         const projectile = projectiles[i]
         projectile.update()
@@ -141,6 +216,17 @@ function animate() {
             projectiles.splice(i,1)
         }
     }
+    // Ast management
+    for (let i = asteroids.length - 1; i >= 0; i--) {
+        const asteroid = asteroids[i]
+        asteroid.update()
+
+        if (asteroid.pos.x + asteroid.r < 0 || asteroid.pos.x > canvas.width || asteroid.pos.y + asteroid.r < 0 || asteroid.pos.y > canvas.height) {
+            asteroids.splice(i,1)
+        }
+    }
+
+    // Player movement
     if (keys.w.pressed) {
         player.vel.x = Math.cos(player.rotation) * SPEED
         player.vel.y = Math.sin(player.rotation) * SPEED
